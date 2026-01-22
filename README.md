5d9ec8543f38495cd2293ad0f15792e21358a16a
25f2001422 <25f2001422@ds.study.iitm.ac.in>
2026-01-22 08:14:49 +0530

    changes in main

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 25f2001422_git_challenge_lab
+In this i am going to write 3 functions in a main function.
---
+++ main.py
@@ -0,0 +1,12 @@
+def sum(n):
+    #Returns the list of natural numbers
+    #Code here
+
+
+def diff(n):
+    #Returns n-th fibonacci number
+    #Code here
+
+def Greet():
+    #Return true if 'n' is prime, otherwise returns false
+    #Code here
